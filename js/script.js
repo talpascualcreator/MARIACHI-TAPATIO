3ed426badc30b9e49fa0c96dab9057253c6fc3e7
talpascualcreator <147327121+talpascualcreator@users.noreply.github.com>
2024-04-20 17:19:56 -0500

    muchos cambion imagenes creacion de muchas secciones

--- js/script.js
+++ js/script.js
@@ -30,4 +30,99 @@ document.addEventListener('DOMContentLoaded', function() {
   // Eventos para cambiar las imágenes al hacer clic en las flechas
   document.querySelector('.flecha-izquierda').addEventListener('click', imagenAnterior);
   document.querySelector('.flecha-derecha').addEventListener('click', siguienteImagen);
+
+  // Espera a que el DOM esté completamente cargado
+document.addEventListener("DOMContentLoaded", function() {
+  // Función para activar los fuegos artificiales
+  function activarFuegosArtificiales() {
+    fireworks.create({
+      count: 3, // Cantidad de fuegos artificiales
+      colors: ["#ff0000", "#00ff00", "#0000ff"], // Colores de los fuegos artificiales
+      backgroundColor: "transparent" // Fondo transparente para que se superpongan sobre el contenido
+    }).play();
+  }
+
+  // Detecta cuando la animación del texto alcanza el punto deseado
+  var texto = document.querySelector(".logo-texto");
+  texto.addEventListener("animationiteration", function() {
+    activarFuegosArtificiales();
+  });
+});
+
+// carrusel clase de guitarra
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function moveSlide(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide1");
+    const dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
+
+// carrusel clase de tecnica vocal
+
+let slideIndex2 = 1;
+showSlides2(slideIndex2);
+
+function moveSlide2(n) {
+    showSlides2(slideIndex2 += n);
+}
+
+function currentSlide2(n) {
+    showSlides2(slideIndex2 = n);
+}
+
+function showSlides2(n) {
+    let i;
+    const slides = document.getElementsByClassName("slide2");
+    const dots = document.getElementsByClassName("dot2");
+
+    if (n > slides.length) {
+        slideIndex2 = 1;
+    }
+    if (n < 1) {
+        slideIndex2 = slides.length;
+    }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex2 - 1].style.display = "block";
+    dots[slideIndex2 - 1].className += " active";
+}
+ // cambiar automaticamente el año
   
+
+var yearSpan = document.getElementById('year');
+
+// Obtiene el año actual
+var currentYear = new Date().getFullYear();
+
+// Asigna el año actual al contenido del span
+yearSpan.textContent = currentYear;
